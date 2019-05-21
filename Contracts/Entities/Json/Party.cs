25f034a4c86301219ad9564af88e9561f8c8ebe5
Dennis <Dennis@DESKTOP-DINHOGP>
2019-05-21 16:42:04 +0200

    Anlage in Git

---
+++ Contracts/Entities/Json/Party.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Entities.Json
+{
+    public class Party
+    {
+        public int Id { get; set; }
+
+        public string NameShort { get; set; }
+
+        public string NameLong { get; set; }
+    }
+}
